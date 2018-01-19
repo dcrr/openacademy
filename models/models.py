2e2dac0da51567bfd78b3577ebb90e1237cab190
Diana <dianacarolinarojas@gmail.com>
2018-01-19 13:54:06 -0500

    - Se agregó un método onchange para validar la cantidad de asistentes contra la cantidad de asientos. - Se agregó active en visa form de session.

--- models/models.py
+++ models/models.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-
 from odoo import models, fields, api
 
 class Course(models.Model):
@@ -14,20 +13,42 @@ class Session(models.Model):
     _name = 'openacademy.session'
 
     name = fields.Char(required=True)
-    start_date = fields.Date()
-    # (6,2) indica la precisión del número
+    # the default value can be a python literal value (boolean, integer, float, string)
+    # or a function that returns value
+    start_date = fields.Date(default=fields.Date.today)
+    # (6,2) indicates the accuracy of the number
     duration = fields.Float(digits=(6, 2), help="Duration in days")
     seats = fields.Integer(string="Number of seats")
-    instructor_id = fields.Many2one('res.partner', string="Instructor", domain=[('instructor','=',True),('category_id.name','ilike', "Teacher")])
+    active = fields.Boolean(default=True)
+    instructor_id = fields.Many2one('res.partner', string="Instructor", domain=[('instructor','=',True),
+                                                                            ('category_id.name','ilike', "Teacher")])
     course_id = fields.Many2one('openacademy.course', ondelete='cascade', string="Course", required=True)
     attendee_ids = fields.Many2many('res.partner', string="Attendees")
     taken_seats = fields.Float(string="Taken Seats", compute='_taken_seats')
 
+    # specifies the fields on which the taken_seats fields depends to be calculated
     @api.depends('seats', 'attendee_ids')
     def _taken_seats(self):
+        # the object self is a recordset, that is, an ordered collection of records
         for r in self:
-            if not  r.seats:
-                r.taken_seats=0.0
+            if not r.seats:
+                r.taken_seats = 0.0
             else:
-                r.taken_seats=100.0*len(r.attendee_ids)/r.seats
+                r.taken_seats = 100.0*len(r.attendee_ids)/r.seats
 
+    @api.onchange('seats', 'attendee_ids')
+    def _verify_valid_seats(self):
+        if self.seats < 0:
+            return {
+                'warning': {
+                    'title': "Incorrect 'seats' value",
+                    'message': "The number of available seats may not be negative",
+                },
+            }
+        if self.seats < len(self.attendee_ids):
+            return {
+                'warning': {
+                    'title': "Too many attendees",
+                    'message': "Increase seats or remove excess attendees",
+                },
+            }
