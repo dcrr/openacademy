95d35c608819a267c0e788f43913cdae3f42cae3
Diana <dianacarolinarojas@gmail.com>
2018-01-20 13:50:51 -0500

    - Se agregó vista gantt, para modelo session,la cual solo funciona en la  versión enterprise de odoo, ya que requiere l módulo web_gantt. - Se agregó vista graph, para modelo session, que muestra cantidad de  alumnos por sección.

--- models/models.py
+++ models/models.py
@@ -33,7 +33,6 @@ class Course(models.Model):
         default['name'] = new_name
         return super(Course, self).copy(default)
 
-
 class Session(models.Model):
     _name = 'openacademy.session'
 
@@ -53,6 +52,8 @@ class Session(models.Model):
     # the inverse function makes the field writable, and allows moving the sessions
     # (via drag and drop) in the calendar view
     end_date = fields.Date(string="End Date", store=True, compute='_get_end_date', inverse='_set_end_date')
+    hours = fields.Float(string="Duration in hours", compute='_get_hours', inverse='_set_hours')
+    attendees_count = fields.Integer(string="Attendees count", compute='_get_attendees_count', store=True)
 
     # specifies the fields on which the taken_seats fields depends to be calculated
     @api.depends('seats', 'attendee_ids')
@@ -117,3 +118,18 @@ class Session(models.Model):
             start_date = fields.Datetime.from_string(r.start_date)
             end_date = fields.Datetime.from_string(r.end_date)
             r.duration = (end_date - start_date).days + 1
+
+    @api.depends('duration')
+    def _get_hours(self):
+        for r in self:
+            r.hours = r.duration * 24
+
+    def _set_hours(self):
+        for r in self:
+            r.duration = r.hours / 24
+
+    @api.depends('attendee_ids')
+    def _get_attendees_count(self):
+        """ Count the attendees numbers in the session """
+        for r in self:
+            r.attendees_count = len(r.attendee_ids)
