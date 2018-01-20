9b637c101a10ef362e6443dc0fe0591d8533a4b6
Diana <dianacarolinarojas@gmail.com>
2018-01-19 21:07:52 -0500

    - Se agregó vista calendar y campo end_date calculado y editable (con  función inverse) en modelo session.

--- models/models.py
+++ models/models.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from odoo import models, fields, api, exceptions
+from datetime import timedelta
 
 class Course(models.Model):
     _name = 'openacademy.course'
@@ -49,12 +50,14 @@ class Session(models.Model):
     course_id = fields.Many2one('openacademy.course', ondelete='cascade', string="Course", required=True)
     attendee_ids = fields.Many2many('res.partner', string="Attendees")
     taken_seats = fields.Float(string="Taken Seats", compute='_taken_seats')
+    # the inverse function makes the field writable, and allows moving the sessions
+    # (via drag and drop) in the calendar view
+    end_date = fields.Date(string="End Date", store=True, compute='_get_end_date', inverse='_set_end_date')
 
     # specifies the fields on which the taken_seats fields depends to be calculated
     @api.depends('seats', 'attendee_ids')
     def _taken_seats(self):
-        """Calculate the value of the taken_seats field
-           Returns the value of taken_seats field"""
+        """Calculate the value of the taken_seats field"""
         # the object self is a recordset, that is, an ordered collection of records
         for r in self:
             if not r.seats:
@@ -89,3 +92,28 @@ class Session(models.Model):
         for r in self:
             if r.instructor_id and r.instructor_id in r.attendee_ids:
                 raise exceptions.ValidationError("A session's instructor can't be an attendee")
+
+    @api.depends('start_date', 'duration')
+    def _get_end_date(self):
+        """Calculate the value of the end_date field"""""
+        for r in self:
+            if not (r.start_date and r.duration):
+                r.end_date = r.start_date
+                continue
+            # Add duration to start_date, but: Monday + 5 days = Saturday, so
+            # subtract one second to get on Friday instead
+            start = fields.Datetime.from_string(r.start_date)
+            duration = timedelta(days=r.duration, seconds=-1)
+            r.end_date = start + duration
+
+    def _set_end_date(self):
+        """Calculate the value of the duration field
+            when end_date field is modified"""""
+        for r in self:
+            if not (r.start_date and r.end_date):
+                continue
+            # Compute the difference between dates, but: Friday - Monday = 4 days,
+            # so add one day to get 5 days instead
+            start_date = fields.Datetime.from_string(r.start_date)
+            end_date = fields.Datetime.from_string(r.end_date)
+            r.duration = (end_date - start_date).days + 1
